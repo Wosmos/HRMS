3755b8cee8cc4253d584b6e05df3976deecadbea
Muhammad Wasif Malik <m.wasifmalik17@gmail.com>
2025-06-21 16:55:25 +0500

    dashboard redirect behaviour fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,39 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/lib/theme-provider"
-import { AuthProvider } from "@/lib/auth-context"
-import { Layout } from "@/components/layout"
+import type React from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/lib/theme-provider';
+import { AuthProvider } from '@/lib/auth-context';
+import { Layout } from '@/components/layout';
+import { usePathname } from 'next/navigation';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "NEXTSOFT HRMS",
-  description: "Human Resource Management System",
-    generator: 'v0.dev'
-}
+  title: 'NEXTSOFT HRMS',
+  description: 'Human Resource Management System',
+  generator: 'v0.dev',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
+  const pathname =
+    typeof window !== 'undefined' ? window.location.pathname : '';
+  const isAuthPage = ['/login', '/register', '/forgot-password'].includes(
+    pathname
+  );
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider defaultTheme="system" storageKey="hrms-ui-theme">
-          <AuthProvider> <Layout>{children}</Layout></AuthProvider>
+        <ThemeProvider defaultTheme='system' storageKey='hrms-ui-theme'>
+          <AuthProvider>
+            {isAuthPage ? children : <Layout>{children}</Layout>}
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
